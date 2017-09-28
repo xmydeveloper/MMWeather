c86077bb69d76d9c61b59f6d1e2b90b2c7c6ec50
xmydeveloper <xmydeveloper@163.com>
2017-09-28 15:45:48 +0800

    提交人：xmy 提交内容：在工具类中HttpUtils中添加请求数据方法、在Utilty中添加省市县数据处理方法

---
+++ app/src/main/java/com/xlw/mmweather/util/HttpUtils.java
@@ -0,0 +1,16 @@
+package com.xlw.mmweather.util;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by xmydeveloper on 2017/9/28/0028.
+ */
+public class HttpUtils {
+    public static void sendOkHttpRequest(String address, Callback callback){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+}
