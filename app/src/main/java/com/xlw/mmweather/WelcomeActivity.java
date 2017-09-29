b92d683a72ff2d59c3cae359a5b518593049ad73
xmydeveloper <xmydeveloper@163.com>
2017-09-29 17:56:00 +0800

    提交人：xmy 提交内容：添加天气欢迎界面

---
+++ app/src/main/java/com/xlw/mmweather/WelcomeActivity.java
@@ -0,0 +1,39 @@
+package com.xlw.mmweather;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+
+/**
+ * Created by xmydeveloper on 2017/9/29/0029.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                finish();
+            }
+        }, 1500);
+
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return true;
+    }
+}
